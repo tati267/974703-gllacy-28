5237501405af1af16c0a2f0713dfd37bdaec6118
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2020-06-07 20:27:32 +1000

    исправление CSS, классов

--- js/pin.js
+++ js/pin.js
@@ -3,17 +3,17 @@
          var myMap,
              myPlacemark;
      
-         function init(){     
+         function init() {     
              myMap = new ymaps.Map ("map", {
                  center: [59.93862734, 30.32282994],
                  zoom: 17
-             });
+           }  );
             
              myPlacemark = new ymaps.Placemark([55.75399400, 37.62209300], {
                  hintContent: 'Gllacy',
                  balloonContent: 'г. Санкт-Петербург, ул. Большая Конюшенная 19/8'
-             }),
+           }  ),
      
              myMap.geoObjects.add(myPlacemark);
      
-         }
+       }  
