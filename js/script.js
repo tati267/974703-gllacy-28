24883138106bb6271fac7d9bcf1cc9f3e9b280c7
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2020-06-12 22:25:22 +1000

    проблемы с отображением заголовка

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+//Slider
+var btn1 = document.querySelector("#btn1");
+var btn2 = document.querySelector("#btn2");
+var btn3 = document.querySelector("#btn3");
+var sitewrapper = document.querySelector(".site-wrapper");
+var slidercontrols = document.querySelector(".slider-controls");
+
+btn1.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  sitewrapper.classList.add("site-wrapper-1");
+  slidercontrols.classList.add("current");
+});
+
+btn2.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  sitewrapper.classList.add("site-wrapper-2");
+  slidercontrols.classList.add("current");
+});
+
+btn3.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  sitewrapper.classList.add("site-wrapper-3");
+  slidercontrols.classList.add("current");
+});
+
+
+//Popup
 var link = document.querySelector(".contacts-block-button");
 var popup = document.querySelector(".popup");
 var popupwrap = document.querySelector(".overlay");
@@ -8,14 +35,14 @@ link.addEventListener("click", function (evt) {
 evt.preventDefault();
 popup.classList.add("popup-show");
 popupwrap.classList.add("overlay-show");
-  });
+});
 
 popupclose.addEventListener("click", function (evt) {
 evt.preventDefault();
 popup.classList.remove("popup-show");
 popupwrap.classList.remove("overlay-show");
 popup.classList.remove("popup-error");
-  });
+});
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
