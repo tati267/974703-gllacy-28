9eb6c184c01a7809ddebd3fbf92307af8614a9ed
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2020-06-13 13:08:31 +1000

    доработка js

--- js/script.js
+++ js/script.js
@@ -3,22 +3,34 @@ var btn1 = document.querySelector("#btn1");
 var btn2 = document.querySelector("#btn2");
 var btn3 = document.querySelector("#btn3");
 var sitewrapper = document.querySelector(".site-wrapper");
+var slidershow = document.querySelector(".slider-show");
 var slidercontrols = document.querySelector(".slider-controls");
+
 
 btn1.addEventListener("click", function (evt) {
   evt.preventDefault();
-  sitewrapper.classList.remove("site-wrapper-2");
-  sitewrapper.classList.remove("site-wrapper-3");
+  slidershow.classList.remove("site-wrapper-2");
+  slidershow.classList.remove("site-wrapper-3");
   sitewrapper.classList.add("site-wrapper-1");
-  slidercontrols.classList.add("current");
+  sitewrapper.classList.remove("site-wrapper-ice2");
+  sitewrapper.classList.remove("site-wrapper-ice3");
+  sitewrapper.classList.add("site-wrapper-ice1");
+  btn2.classList.remove("current");
+  btn3.classList.remove("current");
+  btn1.classList.add("current");
 });
 
 btn2.addEventListener("click", function (evt) {
   evt.preventDefault();
   sitewrapper.classList.remove("site-wrapper-1");
   sitewrapper.classList.remove("site-wrapper-3");
-  slidercontrols.classList.add("current");
   sitewrapper.classList.add("site-wrapper-2");
+  sitewrapper.classList.remove("site-wrapper-ice1");
+  sitewrapper.classList.remove("site-wrapper-ice3");
+  sitewrapper.classList.add("site-wrapper-ice2");
+  btn1.classList.remove("current");
+  btn3.classList.remove("current");
+  btn2.classList.add("current");
 });
 
 btn3.addEventListener("click", function (evt) {
@@ -26,7 +38,12 @@ btn3.addEventListener("click", function (evt) {
   sitewrapper.classList.remove("site-wrapper-1");
   sitewrapper.classList.remove("site-wrapper-2");
   sitewrapper.classList.add("site-wrapper-3");
-  slidercontrols.classList.add("current");
+  sitewrapper.classList.remove("site-wrapper-ice1");
+  sitewrapper.classList.remove("site-wrapper-ice2");
+  sitewrapper.classList.add("site-wrapper-ice3");
+  btn1.classList.remove("current");
+  btn2.classList.remove("current");
+  btn3.classList.add("current");
 });
 
 
